04694424d66be26807acba776e15b84a7adca5aa
Event Management <sealeopard@realmbot.dev>
2026-02-12 17:23:05 +0530

    intialized project

---
+++ src/main/java/com/college/eventmanagement/model/User.java
@@ -0,0 +1,47 @@
+package com.college.eventmanagement.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private OrgType orgType;
+
+    @Column(nullable = false)
+    private String orgName;
+}
---
+++ src/main/java/com/college/eventmanagement/model/enums.java
@@ -0,0 +1,15 @@
+package com.college.eventmanagement.model;
+
+enum Role {
+    ADMIN,
+    STUDENT,
+    FACULTY,
+    HOD,
+    MANAGEMENT
+}
+
+enum OrgType {
+    DEPARTMENT,
+    CLUB,
+    SOCIETY
+}
