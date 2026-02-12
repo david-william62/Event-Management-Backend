25ebbfc1d1a8862a0542dcd573b517eeb3908aeb
Event Management <sealeopard@realmbot.dev>
2026-02-12 17:31:03 +0530

    added user model

--- src/main/java/com/college/eventmanagement/model/User.java
+++ src/main/java/com/college/eventmanagement/model/User.java
@@ -1,10 +1,14 @@
 package com.college.eventmanagement.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -44,4 +48,21 @@ public class User {
 
     @Column(nullable = false)
     private String orgName;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
